1b9729f25055fd116c16c29f4c1d7f10ab0760cc
Derick Bailey <derickbailey@gmail.com>
2009-10-28 11:14:43 -0500

    adding beta version of albacore. updating rake build process to output from msbuild into correct folder

--- rakefile.rb
+++ rakefile.rb
@@ -1,53 +1,52 @@
-task :default => ['uow:coveragereport']
+require 'albacore'
 
-namespace :albacore do	
-	require 'lib/albacore'
+@@xml_coverage = "codecoverage/coverage.xml"
+@@coverage_report = "codecoverage/report/"
+@@output_folder = File.join(File.dirname(__FILE__), "build")
 
-	@@xml_coverage = "codecoverage/coverage.xml"
-	@@coverage_report = "codecoverage/report/"
+task :default => :coveragereport
 
-	desc "Generate version information"
-	Albacore::AssemblyInfoTask.new(:versioninfo) do |asm|
-		asm.version = "0.1.0.0"
-		asm.product_name = "UnitOfWork"
-		asm.copyright = "Copyright (C)2009 Derick Bailey. All Rights Reserved."
-		asm.output_file = "src/VersionInfo.cs"
-	end
+desc "Generate version information"
+Albacore::AssemblyInfoTask.new(:versioninfo) do |asm|
+	asm.version = "0.1.0.0"
+	asm.copyright = "Copyright (C)2009 Derick Bailey. All Rights Reserved."
+	asm.output_file = "src/VersionInfo.cs"
+end
+
+desc "Build the UnitOfWork solution"
+Albacore::MSBuildTask.new(:build => :versioninfo) do |msb|
+	msb.log_level = :verbose
+	msb.properties = {:configuration => :Release, :OutputPath => @@output_folder.gsub("/", "\\")}
+	msb.targets [:Clean, :Build]
+	msb.solution = "src/UoW.sln"
+end
+
+desc "Run code coverage analysis"
+Albacore::NCoverConsoleTask.new(:coverageanalysis => :build) do |ncc|
+	ncc.log_level = :verbose
+	ncc.path_to_command = "tools/NCover-v3.3/NCover.Console.exe"
+	ncc.output = {:xml => @@xml_coverage}
+	ncc.working_directory = "build/"
 	
-	desc "Build the UnitOfWork solution"
-	Albacore::MSBuildTask.new(:build => :versioninfo) do |msb|
-		msb.properties :configuration => :Release
-		msb.targets [:Clean, :Build]
-		msb.solution = "src/UoW.sln"
-	end
+	nunit = NUnitTestRunner.new("tools/NUnit-v2.5/nunit-console.exe")
+	nunit.log_level = :verbose
+	nunit.assemblies << "assemblies/TestSolution.Tests.dll"
+	nunit.options << '/noshadow'
 	
-	desc "Run code coverage analysis"
-	Albacore::NCoverConsoleTask.new(:coverageanalysis => :build) do |ncc|
-		ncc.log_level = :verbose
-		ncc.path_to_command = "tools/NCover-v3.3/NCover.Console.exe"
-		ncc.output = {:xml => @@xml_coverage}
-		ncc.working_directory = "build/"
-		
-		nunit = NUnitTestRunner.new("tools/NUnit-v2.5/nunit-console.exe")
-		nunit.log_level = :verbose
-		nunit.assemblies << "assemblies/TestSolution.Tests.dll"
-		nunit.options << '/noshadow'
-		
-		ncc.testrunner = nunit
-	end	
+	ncc.testrunner = nunit
+end	
+
+desc "Run code coverage report"
+Albacore::NCoverReportTask.new(:coveragereport => :coverageanalysis) do |ncr|
+	ncr.path_to_command = "tools/NCover-v3.3/NCover.Reporting.exe"
+	ncr.coverage_files << @@xml_coverage
 	
-	desc "Run code coverage report"
-	Albacore::NCoverReportTask.new(:coveragereport => :coverageanalysis) do |ncr|
-		ncr.path_to_command = "tools/NCover-v3.3/NCover.Reporting.exe"
-		ncr.coverage_files << @@xml_coverage
-		
-		fullcoveragereport = NCover::FullCoverageReport.new
-		fullcoveragereport.output_path = "codecoverage/report/"
-		ncr.reports << fullcoveragereport
-		
-		ncr.required_coverage << NCover::BranchCoverage.new(:minimum => 50)
-		ncr.required_coverage << NCover::SymbolCoverage.new(:minimum => 50)
-		ncr.required_coverage << NCover::MethodCoverage.new(:minimum => 50)
-		ncr.required_coverage << NCover::CyclomaticComplexity.new(:maximum => 100)
-	end
+	fullcoveragereport = NCover::FullCoverageReport.new
+	fullcoveragereport.output_path = @@coverage_report
+	ncr.reports << fullcoveragereport
+	
+	ncr.required_coverage << NCover::BranchCoverage.new(:minimum => 50)
+	ncr.required_coverage << NCover::SymbolCoverage.new(:minimum => 50)
+	ncr.required_coverage << NCover::MethodCoverage.new(:minimum => 50)
+	ncr.required_coverage << NCover::CyclomaticComplexity.new(:maximum => 100)
 end
--- src/VersionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-[assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: AssemblyCopyrightAttribute("Copyright (c)2009 Derick Bailey. All Rights Reserved.")]
-[assembly: AssemblyTrademarkAttribute("")]
-
